d586acfbe3ff636a760807ab53a487d6a5e097a2
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-23 19:33:18 +0200

    add

--- src/main.js
+++ src/main.js
@@ -80,9 +80,3 @@ const loadMoreImages = loadMoreButton.addEventListener(
     }
   }
 );
-
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
-
-// window.scrollBy({ top: cardHeight * 2, behavior: 'smooth' });
