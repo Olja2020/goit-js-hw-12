ad6edc3da2e3cef01a68c71978be226a7dbd339e
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-23 21:28:52 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,7 +7,6 @@ import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 import { per_page } from '../main.js';
-const totalPages = Math.ceil(100 / per_page);
 
 const loadMoreButton = document.querySelector('.load');
 const button = document.querySelector('.searchButton');
@@ -16,10 +15,11 @@ const loader = document.querySelector('.loader');
 import { renderUsers } from './render-functions';
 import { page } from '../main.js';
 import { inputSearchValue } from '../main.js';
+const totalPages = Math.ceil(100 / per_page);
 
 export async function fetchImages() {
   const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`;
-  debugger;
+
   if (inputSearchValue) {
     try {
       const response = await axios.get(url);
